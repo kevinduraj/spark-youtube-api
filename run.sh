1a92d0f1ea4e4d2cb3214cc54c6b21adfc3816e0
Kevin Duraj <kduraj@gmail.com>
2016-12-15 19:06:57 -0800

    2016-12-15 19:06

--- run.sh
+++ run.sh
@@ -15,7 +15,7 @@ spark-submit                                        \
   --driver-memory   32G                             \
   --executor-memory 16G                             \
   target/scala-2.11/spark-cassandra_2.11-1.0.jar    \
-  "count_null"
+  "count_not_null"
 
 #-----------------------------------------------------------------------------------#
 elif [ "$1" == "2" ] 2>/dev/null; then
@@ -26,10 +26,21 @@ spark-submit                                        \
   --driver-memory   32G                             \
   --executor-memory 16G                             \
   target/scala-2.11/spark-cassandra_2.11-1.0.jar    \
-  "count_all"
+  "count_null"
 
 #-----------------------------------------------------------------------------------#
 elif [ "$1" == "3" ] 2>/dev/null; then
+
+spark-submit                                        \
+  --class "YoutubeVideos"                           \
+  --master local[16]                                \
+  --driver-memory   32G                             \
+  --executor-memory 16G                             \
+  target/scala-2.11/spark-cassandra_2.11-1.0.jar    \
+  "count_all"
+
+#-----------------------------------------------------------------------------------#
+elif [ "$1" == "4" ] 2>/dev/null; then
 
 spark-submit                                        \
   --class "YoutubeVideos"                           \
@@ -44,10 +55,10 @@ else
 
   echo "+--------------------------------------------------+"
   echo "| ./run.sh 0 -- mvn clean install                  |"
-  echo "| ./run.sh 1 -- count_null                         |"
-  echo "| ./run.sh 2 -- count_all                          |"
-  echo "| ./run.sh 3 -- export_null                        |"
-  echo "| ./run.sh 9 -- s3cmd ls s3://fresno-youtube-json  |"
+  echo "| ./run.sh 1 -- count_not_null                     |"
+  echo "| ./run.sh 2 -- count_null                         |"
+  echo "| ./run.sh 3 -- count_all                          |"
+  echo "| ./run.sh 4 -- export_null                        |"
   echo "+--------------------------------------------------+"
 
 fi
--- src/main/scala/YoutubeVideos.scala
+++ src/main/scala/YoutubeVideos.scala
@@ -18,17 +18,37 @@ object YoutubeVideos {
     val locale = new java.util.Locale("us", "US")
     val formatter = java.text.NumberFormat.getIntegerInstance(locale)
 
+    /*-------------------------------------------------------------------------------------------------------------------------------------*/
     def main(args: Array[String]) {
 
-        if(args(0) == "count_null") {
+        if(args(0) == "count_not_null") {
+            count_not_null();
+        } else if(args(0) == "count_null" ) {
             count_null();
         } else if(args(0) == "count_all" ) {
             count_all();
         } else if(args(0) == "export_null")  {
             export_null();
+        } else if(args(0) == "export_not_null")  {
+            export_not_null();
         }
     }
 
+    /*-------------------------------------------------------------------------------------------------------------------------------------*/
+    def count_not_null() {
+
+        val spark = SparkSession.builder().appName("YoutubeVideos").config("spark.some.config.option", "some-value").getOrCreate()
+        import spark.implicits._
+
+        val df1 = spark.read.format("org.apache.spark.sql.cassandra").options(Map( "table" -> "video2", "keyspace" -> "youtube" )).load()
+        df1.printSchema()
+        df1.createOrReplaceTempView("video")
+
+        //val df2 = spark.sql("SELECT video_id, video_title, ts_data_update FROM video WHERE ts_data_update <= '2016-12-04 00:00:00+0000'")
+        val df2 = spark.sql("SELECT count(video_id) ts_stats_update FROM video WHERE ts_stats_update IS NOT NULL")
+        df2.show()
+    }
+    /*-------------------------------------------------------------------------------------------------------------------------------------*/
     def count_null() {
 
         val spark = SparkSession.builder().appName("YoutubeVideos").config("spark.some.config.option", "some-value").getOrCreate()
@@ -43,6 +63,7 @@ object YoutubeVideos {
         df2.show()
     }
 
+    /*-------------------------------------------------------------------------------------------------------------------------------------*/
     def count_all() {
 
         val spark = SparkSession.builder().appName("YoutubeVideos").config("spark.some.config.option", "some-value").getOrCreate()
@@ -58,6 +79,7 @@ object YoutubeVideos {
 
     }
 
+    /*-------------------------------------------------------------------------------------------------------------------------------------*/
     def export_null() {
 
         val spark = SparkSession.builder().appName("YoutubeVideos").config("spark.some.config.option", "some-value").getOrCreate()
@@ -69,6 +91,7 @@ object YoutubeVideos {
 
         //val df2 = spark.sql("SELECT video_id, video_title, ts_data_update FROM video WHERE ts_data_update <= '2016-12-04 00:00:00+0000'")
         val df2 = spark.sql("SELECT video_id FROM video WHERE video_title IS NULL")
+        //val df2 = spark.sql("SELECT video_id FROM video WHERE video_title = '37a6259cc0c1dae299a7866489dff0bd'")
         println("NULL = " + df2.count())
         df2.show(25, false)
 
